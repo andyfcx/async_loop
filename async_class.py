ee7d340aed5e07d6e59c149b20dc71f392550b32
andyfcx <dreamcastle.andy@gmail.com>
2022-06-14 13:59:10 +0800

    chore: GA

--- async_class.py
+++ async_class.py
@@ -1,8 +1,7 @@
 import asyncio
-import time
+
 
 class TypeOne():
-    
     def __init__(self, param):
         loop = asyncio.get_event_loop()
         self.member = loop.run_until_complete(create_member(param))
@@ -16,12 +15,12 @@ class TypeTwo():
     def __init__(self, param):
         asyncio.run(self.__ainit__(param))
         print("ASYNC DONE")
-    
-    
+
 
 async def create_member(x):
     await asyncio.sleep(1)
     return x**2
+
 
 def main():
     t1 = TypeOne(10)
@@ -29,6 +28,6 @@ def main():
     print(t1.member)
     print(t2.member)
 
+
 if __name__ == "__main__":
     main()
-    # asyncio.run(main())
